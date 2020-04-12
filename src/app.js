9e4798c56bdc5962a4a7cb9189bf90e689bcf41c
oluwamayowaf <e_fadairo@yahoo.com>
2020-04-12 11:24:28 +0100

    Backend API

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import express from 'express';
 import 'express-async-errors';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import routes from './routes';
 import responseTime from 'response-time';
+import routes from './routes';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(responseTime())
+app.use(responseTime());
 app.use(routes);
 
 
 const port = 5000;
 
-app.listen(port, () => console.log(`Server is listening on localhost:${port}`))
+app.listen(port, () => console.log(`Server is listening on localhost:${port}`));
 
 export default app;
