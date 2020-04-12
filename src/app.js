9f6ca0a53dabb7dc3f8251dddc7f880024652b4e
oluwamayowaf <e_fadairo@yahoo.com>
2020-04-12 12:15:17 +0100

    Backend API

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(responseTime());
 app.use(routes);
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is listening on localhost:${port}`));
 
